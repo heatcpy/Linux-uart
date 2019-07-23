cd9e6c780205a8e541e2ab72dd1ddf218979eb1f
heatcpy <heatcpy@163.com>
2019-07-23 17:27:04 +0800

    add linux uart demo

---
+++ uart/include.h
@@ -0,0 +1,33 @@
+#ifndef __INCLUDE_H_
+#define __INCLUDE_H_
+
+
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>                                                      
+//#include <sqlite3.h>
+//#include "/usr/local/include/json/json.h"
+
+//串口相关的头文件     
+#include<fcntl.h>      /*文件控制定义*/  
+#include<termios.h>    /*PPSIX 终端控制定义*/  
+#include<errno.h>      /*错误号定义*/  
+#include<string.h>  
+
+
+#endif
+
+
